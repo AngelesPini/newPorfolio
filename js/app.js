258198ac641f4d17eccea850e7ea221a514514d1
angelespini <angelesbpini@gmail.com>
2022-07-27 14:09:03 -0300

    new porfolio

---
+++ js/app.js
@@ -0,0 +1,41 @@
+
+let textoShow = document.querySelector("#textoShow");
+
+function gliderMobileFunction (div, description) {
+
+    switch (div){
+        case 1:
+            textoShow.innerHTML = `<p class=txt> ${description}</p>`
+            document.getElementById('buttonUno').style.backgroundColor = '#FFC002';
+            document.getElementById('buttonDos').style.backgroundColor = '#b9b9b9';
+            document.getElementById('buttonTres').style.backgroundColor = '#b9b9b9';
+            document.getElementById('buttonCuatro').style.backgroundColor = '#b9b9b9';
+            break;
+        case 2:
+            textoShow.innerHTML = `<p class=txt> ${description}</p>`
+            document.getElementById('buttonUno').style.backgroundColor = '#b9b9b9';
+            document.getElementById('buttonDos').style.backgroundColor = '#FFC002';
+            document.getElementById('buttonTres').style.backgroundColor = '#b9b9b9';
+            document.getElementById('buttonCuatro').style.backgroundColor = '#b9b9b9';
+            break;
+        case 3:
+            textoShow.innerHTML = `<p class=txt> ${description}</p>`
+            document.getElementById('buttonUno').style.backgroundColor = '#b9b9b9';
+            document.getElementById('buttonDos').style.backgroundColor = '#b9b9b9';
+            document.getElementById('buttonTres').style.backgroundColor = '#FFC002';
+            document.getElementById('buttonCuatro').style.backgroundColor = '#b9b9b9';
+            break;
+        case 4:
+            textoShow.innerHTML = `<p class=txt> ${description}</p>`
+            document.getElementById('buttonUno').style.backgroundColor = '#b9b9b9';
+            document.getElementById('buttonDos').style.backgroundColor = '#b9b9b9';
+            document.getElementById('buttonTres').style.backgroundColor = '#b9b9b9';
+            document.getElementById('buttonCuatro').style.backgroundColor = '#FFC002';
+            break;
+        default:
+            console.log('uno');
+            document.getElementById('buttonUno').style.backgroundColor = '#F6007C';
+            break;
+
+    }
+}
